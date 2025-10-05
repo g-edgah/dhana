7e4f017545617495c9cecb29004c6af32dcd9464
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-10-05 14:10:52 +0300

    controller

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,15 +1,13 @@
 import express from 'express'
-import {getNote, getAllNotes, updateNote, deleteNote, deleteAllNotes} from '../controller/notesController.js'
+import {getNote, getAllNotes, createNote, updateNote, deleteNote, deleteAllNotes} from '../controller/notesController.js'
 
 const notesRouter = express.Router();
 
-notesRouter.get("/", (req, res) => {
-    res.status(200).send("notes obtained")
-})
+notesRouter.get("/", getAllNotes)
 
 notesRouter.get("/:id", getNote)
 
-notesRouter.post("/addnote", getAllNotes)
+notesRouter.post("/addnote", createNote)
 
 notesRouter.put("/updatenote/:id", updateNote)
 
