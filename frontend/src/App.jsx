086ed938c0d43d1a0cf5e81da32524f2f3773f3e
g-edgah <172597903+g-edgah@users.noreply.github.com>
2025-10-08 00:35:24 +0300

    notecard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,8 +11,8 @@ import toast from 'react-hot-toast'
 function App() {
   
   return (
-   <div data-theme="forest">
-    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 " />
+   <div className="relative h-full w-full">
+    <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]" />
     <Routes>
       <Route path='/' element={<HomePage />}></Route>
       <Route path='/note/:id' element={<NoteDetailPage />}></Route>
